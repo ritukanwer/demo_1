8b307abefe1824f4817355a0eb955bf65912e128
Ritu <ritunarukarajput.21@gmail.com>
2025-12-15 19:40:56 +0530

    try_with_else

---
+++ chapter_12_walrusoperators/06_try_else.py
@@ -0,0 +1,14 @@
+
+try:
+    a = int(input("enter a number :-"))
+    print(a)
+
+except:
+    print("print a valid int")
+
+else:
+    print("i am inside else")
+
+
+    # if try run successfully then it goes to the else otherwise it doesn't ot inside else:
+    
