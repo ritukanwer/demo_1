c1e63dfd6f389a2a12760498b55f65253c72bd3c
Ritu <ritunarukarajput.21@gmail.com>
2025-12-18 22:22:56 +0530

    read files

---
+++ chapter_12_walrusoperators_practiceset/01_problem.py
@@ -0,0 +1,21 @@
+# write a program to open three files 1.txt,2.txt and 3.txt  if any these files ore not present , a message
+# without existing the program must be printed prompting the same 
+
+try:
+    with open("1.txt","r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+try:
+    with open("2.txt","r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+
+try:
+    with open("2.txt","r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
